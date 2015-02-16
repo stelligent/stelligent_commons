0fdfc6443a4e264fb7453e141d547d43b15f4191
Sean Leavey <sean.leavey@stelligent.com>
2015-02-16 11:44:25 -0500

    Added Matrix Role Based Security on Jenkins

--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/default.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/default.rb
@@ -1,3 +1,4 @@
 include_recipe 'jenkins-configuration::install_plugins'
 include_recipe 'jenkins-configuration::git'
 include_recipe 'jenkins-configuration::generate-cert'
+include_recipe 'jenkins-configuration::jenkins-security'
