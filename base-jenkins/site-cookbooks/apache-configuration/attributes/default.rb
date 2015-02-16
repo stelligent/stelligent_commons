c36184d970323dcbd54d8b0e2508ea99c87e11f8
Eric Kascic <eric.kascic@stelligent.com>
2015-02-16 11:26:08 -0500

    override the apache listening ports properly

--- base-jenkins/site-cookbooks/apache-configuration/attributes/default.rb
+++ base-jenkins/site-cookbooks/apache-configuration/attributes/default.rb
@@ -1,5 +1,6 @@
 default['jenkins']['http_proxy']['host_aliases'] = []
 default['jenkins']['http_proxy']['ssl']['listen_port'] = 443
 default['jenkins']['http_proxy']['listen_port'] = 80
-
 default['jenkins']['master']['port'] = 8080
+
+default['apache']['listen_ports'] = %w{80 443}
