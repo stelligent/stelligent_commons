cbeda2357ba419bc182091e24bafd253963c1c70
Eric Kascic <eric.kascic@stelligent.com>
2015-02-15 22:02:37 -0500

    generate the cert for ssl fix code to set permissions on cert and key

--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/default.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/default.rb
@@ -1,2 +1,3 @@
 include_recipe 'jenkins-configuration::install_plugins'
 include_recipe 'jenkins-configuration::git'
+include_recipe 'jenkins-configuration::generate-cert'
--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/generate-cert.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/generate-cert.rb
@@ -1,10 +1,18 @@
-ssl_dir = '/etc/httpd/ssl'
+directory File.dirname(node['jenkins']['http_proxy']['ssl']['cert_path']) do
+  recursive true
+  mode '0500'
+end
 
 bash 'generate a self-signed certificate' do
-
   code <<-END
-    mkdir -p #{ssl_dir}
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '#{node[:jenkins][:cert_subject]}' -keyout #{ssl_dir}/server.key -out #{ssl_dir}/server.crt
-    chmod -R 0500 #{ssl_dir}
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '#{node[:jenkins][:cert_subject]}' -keyout  #{node['jenkins']['http_proxy']['ssl']['key_path']} -out  #{node['jenkins']['http_proxy']['ssl']['cert_path']}
   END
+end
+
+file node['jenkins']['http_proxy']['ssl']['cert_path'] do
+  mode '0500'
+end
+
+file node['jenkins']['http_proxy']['ssl']['key_path'] do
+  mode '0500'
 end
