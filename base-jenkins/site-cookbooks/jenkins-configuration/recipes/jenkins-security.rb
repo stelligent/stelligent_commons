fe21442e8c24fab0077145ff6f1bd7feb42528fc
Sean Leavey <sean.leavey@stelligent.com>
2015-02-16 14:59:25 -0500

    Simplified to just adding an admin only user

--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/jenkins-security.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/jenkins-security.rb
@@ -5,10 +5,7 @@ cookbook_file "script to add Jenkins security and users" do
   path "/tmp/create_maxtrix_sec.groovy"
 end
 
-users = node['jenkins_security']['users'].collect {|user| "#{user[0]} #{user[1]} #{user[2]}"}.each do |args|
-  jenkins_command "add global variables" do
-    command "groovy /tmp/create_maxtrix_sec.groovy #{args}"
-    only_if { node['jenkins_security']['authz_strategy'] == 'matrix' }
-  end
+jenkins_command "add global variables" do
+  command "groovy /tmp/create_maxtrix_sec.groovy"
+  only_if { node['jenkins_security']['authz_strategy'] == 'matrix' }
 end
-
