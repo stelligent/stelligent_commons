7860cf65d30e186ff891aa568a57a2306de9a2e8
Eric Kascic <eric.kascic@stelligent.com>
2015-02-16 08:31:07 -0500

    set up the default ports and allow tweaking these so redirect can play well on vagrant box

--- base-jenkins/site-cookbooks/apache-configuration/attributes/default.rb
+++ base-jenkins/site-cookbooks/apache-configuration/attributes/default.rb
@@ -1,3 +1,5 @@
 default['jenkins']['http_proxy']['host_aliases'] = []
-default['jenkins']['http_proxy']['ssl']['ssl_listen_ports'] = [443]
+default['jenkins']['http_proxy']['ssl']['listen_port'] = 443
+default['jenkins']['http_proxy']['listen_port'] = 80
+
 default['jenkins']['master']['port'] = 8080
