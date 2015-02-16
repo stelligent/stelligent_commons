e7dbd2048b18dcd85f571a238e288da998f365a0
Sean Leavey <sean.leavey@stelligent.com>
2015-02-16 14:05:54 -0500

    updated jenkins matrix security to move groovy code into separate file

--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/jenkins-security.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/jenkins-security.rb
@@ -1,58 +1,14 @@
 # Turn on matrix based authentication
 
-jenkins_script 'setup authentication' do
-  command <<-EOH.gsub(/^ {4}/, '')
-    import hudson.model.Hudson;
-    import hudson.security.GlobalMatrixAuthorizationStrategy;
-    import hudson.security.HudsonPrivateSecurityRealm;
+cookbook_file "script to add Jenkins security and users" do
+  source "create_maxtrix_sec.groovy"
+  path "/tmp/create_maxtrix_sec.groovy"
+end
 
-    if(args.length != 3) {
-      println("no user specified, creating default");
-      user = "admin"
-      password = "dev"
-      role = Hudson.ADMINISTER
-    } else {
-      user = args[0];
-      password = args[1];
-
-      if (args[2] == "read") {
-        role = Hudson.READ
-      } else if (args[2] == "admin") {
-        role = Hudson.ADMINISTER
-      } else {
-        println "${role} is an invalid role, defaulting to read-only."
-        role = Hudson.READ
-      }
-    } 
-
-    def constructMatrixAuthorizationStrategy(user, role) {
-      authzStrategy = new GlobalMatrixAuthorizationStrategy();
-      authzStrategy.add(role, user);
-      return authzStrategy;
-    }
-
-    def configurePrivateSecurityRealm(user, password) {
-      doNotAllowSignup = false;
-      hudsonPrivateSecurityRealm = new HudsonPrivateSecurityRealm(doNotAllowSignup);
-
-      hudsonPrivateSecurityRealm.createAccount(user, password)
-
-      Hudson.instance.setSecurityRealm(hudsonPrivateSecurityRealm);
-      println "Successfully set security realm to Hudson Private Realm without ability to signup";
-    }
-
-    authzStrategy = Hudson.instance.getAuthorizationStrategy();
-    if(!authzStrategy.getClass().equals(GlobalMatrixAuthorizationStrategy.class)) {
-      matrixAuthzStrategy = constructMatrixAuthorizationStrategy(user, role);
-      Hudson.instance.setAuthorizationStrategy(matrixAuthzStrategy);
-      
-      configurePrivateSecurityRealm(user, password);
-
-      Hudson.instance.save();
-    }
-    else {
-      //test deeper?
-    }
-  EOH
+users = node['jenkins_security']['users'].collect {|user| "#{user[0]} #{user[1]} #{user[2]}"}.each do |args|
+  jenkins_command "add global variables" do
+    command "groovy /tmp/create_maxtrix_sec.groovy #{args}"
+    only_if { node['jenkins_security']['authz_strategy'] == 'matrix' }
+  end
 end
 
