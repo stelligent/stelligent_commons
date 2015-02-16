430f76b3606bf447f46449dca7603b202e3b40bd
Sean Leavey <sean.leavey@stelligent.com>
2015-02-16 14:06:49 -0500

    jenkins security groovy script

---
+++ base-jenkins/site-cookbooks/jenkins-configuration/files/create_maxtrix_sec.groovy
@@ -0,0 +1,53 @@
+    import hudson.model.Hudson;
+    import hudson.security.GlobalMatrixAuthorizationStrategy;
+    import hudson.security.HudsonPrivateSecurityRealm;
+
+    role = null
+
+    if(args.length != 3) {
+      println("no user specified, creating default");
+      user = "admin"
+      password = "dev"
+      role = Hudson.ADMINISTER
+    } else {
+      user = args[0];
+      password = args[1];
+
+      if (args[2] == "read") {
+        role = Hudson.READ
+      } else if (args[2] == "admin") {
+        role = Hudson.ADMINISTER
+      } else {
+        println "${role} is an invalid role, defaulting to read-only."
+        role = Hudson.READ
+      }
+    } 
+
+    def constructMatrixAuthorizationStrategy(user, role) {
+      authzStrategy = new GlobalMatrixAuthorizationStrategy();
+      authzStrategy.add(role, user);
+      return authzStrategy;
+    }
+
+    def configurePrivateSecurityRealm(user, password) {
+      doNotAllowSignup = false;
+      hudsonPrivateSecurityRealm = new HudsonPrivateSecurityRealm(doNotAllowSignup);
+
+      hudsonPrivateSecurityRealm.createAccount(user, password)
+
+      Hudson.instance.setSecurityRealm(hudsonPrivateSecurityRealm);
+      println "Successfully set security realm to Hudson Private Realm without ability to signup";
+    }
+
+    authzStrategy = Hudson.instance.getAuthorizationStrategy();
+    if(!authzStrategy.getClass().equals(GlobalMatrixAuthorizationStrategy.class)) {
+      matrixAuthzStrategy = constructMatrixAuthorizationStrategy(user, role);
+      Hudson.instance.setAuthorizationStrategy(matrixAuthzStrategy);
+      
+      configurePrivateSecurityRealm(user, password);
+
+      Hudson.instance.save();
+    }
+    else {
+      //test deeper?
+    }
