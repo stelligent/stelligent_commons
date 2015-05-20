2d5dce8b66f49a1655357407386b2222b2099770
Jonathan Sywulak <jsywulak@users.noreply.github.com>
2015-05-20 16:26:46 -0400

    Execute commands using bash instead of whatever default shell the Jenkins user has.

--- base-jenkins/site-cookbooks/jenkins-configuration/recipes/git.rb
+++ base-jenkins/site-cookbooks/jenkins-configuration/recipes/git.rb
@@ -11,8 +11,8 @@ end
 
 bash 'setup git for jenkins user' do
   code <<-END
-    su -c 'git config --global user.name #{node[:jenkins_git][:username]}' - jenkins
-    su -c 'git config --global user.email #{node[:jenkins_git][:email]}' - jenkins
+    su -c 'git config --global user.name #{node[:jenkins_git][:username]}' -s /bin/bash - jenkins
+    su -c 'git config --global user.email #{node[:jenkins_git][:email]}' -s /bin/bash - jenkins
   END
 end
 
