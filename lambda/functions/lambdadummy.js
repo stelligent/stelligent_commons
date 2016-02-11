2b5a669faad0dff926d357e3874f39eb61cdef7c
“PaulDuvall” <paul.duvall@stelligent.com>
2016-02-11 10:38:48 -0500

    use lambda for all actions with dummy function that returns sucess

--- lambda/functions/lambdadummy.js
+++ lambda/functions/lambdadummy.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var AWS = require('aws-sdk');
-var http = require('http');
+
+console.log('Loading function');
 
 exports.handler = function(event, context) {
 
@@ -8,11 +9,12 @@ exports.handler = function(event, context) {
     
     // Retrieve the Job ID from the Lambda action
     var jobId = event["CodePipeline.job"].id;
-    
-    // Retrieve the value of UserParameters from the Lambda action configuration in AWS CodePipeline, in this case a URL which will be
-    // health checked by this function.
-    var url = event["CodePipeline.job"].data.actionConfiguration.configuration.UserParameters; 
-    
+
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('value1 =', event.key1);
+    console.log('value2 =', event.key2);
+    console.log('value3 =', event.key3);
+        
     // Notify AWS CodePipeline of a successful job
     var putJobSuccess = function(message) {
         var params = {
@@ -27,58 +29,7 @@ exports.handler = function(event, context) {
         });
     };
     
-    // Notify AWS CodePipeline of a failed job
-    var putJobFailure = function(message) {
-        var params = {
-            jobId: jobId,
-            failureDetails: {
-                message: JSON.stringify(message),
-                type: 'JobFailed',
-                externalExecutionId: context.invokeid
-            }
-        };
-        codepipeline.putJobFailureResult(params, function(err, data) {
-            context.fail(message);      
-        });
-    };
-    
-    
-    // Helper function to make a HTTP GET request to the page.
-    // The helper will test the response and succeed or fail the job accordingly 
-    var getPage = function(url, callback) {
-        var pageObject = {
-            body: '',
-            statusCode: 0,
-            contains: function(search) {
-                return this.body.indexOf(search) > -1;    
-            }
-        };
-        http.get(url, function(response) {
-            pageObject.body = '';
-            pageObject.statusCode = response.statusCode;
-            
-            response.on('data', function (chunk) {
-                pageObject.body += chunk;
-            });
-            
-            response.on('end', function () {
-                callback(pageObject);
-            });
-            
-            response.resume(); 
-        }).on('error', function(error) {
-            // Succeed the job
-            putJobSuccess("Tests passed.");    
-        });           
-    };
-    
-    getPage(url, function(returnedPage) {
-        try {
-            // Succeed the job
-            putJobSuccess("Tests passed.");
-        } catch (ex) {
-            // Succeed the job
-            putJobSuccess("Tests passed.");   
-        }
-    });     
+    // Succeed the job
+    putJobSuccess("Tests passed.");   
+  
 };
